0b3e7df70fd27eaab26f4fe39e9642fb9ddb5af4
Riya Rastogi <riya02rastogi@gmail.com>
2025-05-20 19:01:18 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import taskRoutes from './routes/taskRoutes.js';
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://taupe-horse-00605b.netlify.app/'  
+}));
 app.use(bodyParser.json());
 
 app.use('/tasks', taskRoutes);
